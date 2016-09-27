0b2e9b71cb3d3d2a91a0cad8dc040452f2fe9eeb
DSnimshchikov <dsnimshikov@gmail.com>
2016-09-28 01:15:25 +0300

    fixes
    * .gitignore
    * codestyle

--- src/ru/bstu/iitus/vt41/gibkinyuriy/Main.java
+++ src/ru/bstu/iitus/vt41/gibkinyuriy/Main.java
@@ -1,66 +1,58 @@
-
 package ru.bstu.iitus.vt41.gibkinyuriy;
 
 import java.util.Scanner;
-public class Main
-{
-    public static void main(String[] args)
-    {
+
+public class Main {
+    public static void main(String[] args) {
         // write your code here
         int count;//количество человек
         String Name;//имя и фамилия человека
-        Scanner in=new Scanner(System.in);
+        Scanner in = new Scanner(System.in);
         System.out.println("Input Count member:");
-        count=in.nextInt();//ввод членов Person
-        Person[]pr=new Person[count];
-        for(int i=0;i<count;i++)
-        {
+        count = in.nextInt();//ввод членов Person
+        Person[] pr = new Person[count];
+        for (int i = 0; i < count; i++) {
             System.out.println("Input Name and Surname:");
-            Name=in.nextLine();
-            Name=in.nextLine();//ввод фам имя
+            Name = in.nextLine();
+            Name = in.nextLine();//ввод фам имя
             System.out.println("Input age:");
-            int age=in.nextInt();//ввод возраста
+            int age = in.nextInt();//ввод возраста
             System.out.println("Input type:");
             System.out.println("1-teacher, 2-manager, 3-schoolkid, 4-student");
-            int type=in.nextInt();//ввод типа
-            switch(type)
-            {
-                case 1:
-                    {
-                        pr[i]=new Teacher(age);
-                        pr[i].Name=Name;
-                        pr[i].init(in);
-                        break;
-                    }
-                    case 2:
-                        {
-                            pr[i]=new Manager(age);
-                            pr[i].Name=Name;
-                            pr[i].init(in);
-                            break;
-                        }
-                        case 3:
-                            {
-                                pr[i]=new Schoolkid(age);
-                                pr[i].Name=Name;
-                                pr[i].init(in);
-                                break;
-                            }
-                            case 4:
-                                {
-                                    pr[i]=new Student(age);
-                                    pr[i].Name=Name;
-                                    pr[i].init(in);
-                                    break;
-                                }
-                                default: break;
+            int type = in.nextInt();//ввод типа
+            switch (type) {
+                case 1: {
+                    pr[i] = new Teacher(age);
+                    pr[i].Name = Name;
+                    pr[i].init(in);
+                    break;
+                }
+                case 2: {
+                    pr[i] = new Manager(age);
+                    pr[i].Name = Name;
+                    pr[i].init(in);
+                    break;
+                }
+                case 3: {
+                    pr[i] = new Schoolkid(age);
+                    pr[i].Name = Name;
+                    pr[i].init(in);
+                    break;
+                }
+                case 4: {
+                    pr[i] = new Student(age);
+                    pr[i].Name = Name;
+                    pr[i].init(in);
+                    break;
+                }
+                default:
+                    break;
             }
         }
-        int minAge=pr[0].getAge();//принимаем нач мин возраст
-        int id=0;
-        for (int i=1;i<count;i++)//находим мин возраст и запоминаем номер этого члена
-            if(minAge>pr[i].getAge())
-            {
+        int minAge = pr[0].getAge();//принимаем нач мин возраст
+        int id = 0;
+        for (int i = 1; i < count; i++)//находим мин возраст и запоминаем номер этого члена
+            if (minAge > pr[i].getAge()) {
                 minAge = pr[i].getAge();
                 id = i;
             }
--- src/ru/bstu/iitus/vt41/gibkinyuriy/Person.java
+++ src/ru/bstu/iitus/vt41/gibkinyuriy/Person.java
@@ -1,95 +1,95 @@
 package ru.bstu.iitus.vt41.gibkinyuriy;
-import java.util.Scanner; // импортируем класс
+
+import java.util.Scanner;
 
 /**
  * Created by yuriy on 20.09.2016.
  */
 public abstract class Person {
-    private int Age;
     public String Name;
     public String Member;
     public int Number;
+    private int Age;
+
+    public Person(int age, String member) {
+        this.Age = age;
+        this.Member = member;
+    }
+
     public abstract void init(Scanner scanner);
 
-    public int getAge(){return Age;}
-    public Person(int age, String member)
-    {
-        this.Age=age;
-        this.Member=member;
+    public int getAge() {
+        return Age;
     }
 
     @Override
-    public String toString()
-    {
-        return "Name: " + this.Name + " Age: " + this.Age+ " Member: "+this.Member+" Number: "+this.Number;
+    public String toString() {
+        return "Name: " + this.Name + " Age: " + this.Age + " Member: " + this.Member + " Number: " + this.Number;
     }
 
 
 }
+
 class Student extends Person//класс студент
 {
-    public Student(int age)
-    {
-        super(age,"Student");
+    public Student(int age) {
+        super(age, "Student");
     }
+
     @Override
-    public void init(Scanner scanner)
-    {
+    public void init(Scanner scanner) {
         System.out.println("Input Student ID card:");
-        this.Number=scanner.nextInt();
+        this.Number = scanner.nextInt();
 
     }
 }
 
 class Schoolkid extends Person//класс Школьники
 {
-    public Schoolkid(int age)
-    {
-        super(age,"Schoolkid");
+    public Schoolkid(int age) {
+        super(age, "Schoolkid");
     }
+
     @Override
-    public void init(Scanner scanner)
-    {
+    public void init(Scanner scanner) {
         System.out.println("Input number school:");
-        this.Number=scanner.nextInt();
+        this.Number = scanner.nextInt();
     }
 }
+
 class Cooperator extends Person//класс сотрудники
 {
-    public Cooperator(int age,String member)
-    {
-        super(age,member);
+    public Cooperator(int age, String member) {
+        super(age, member);
     }
+
     @Override
-    public void init(Scanner scanner)
-    {
+    public void init(Scanner scanner) {
     }
 }
 
 class Teacher extends Cooperator//класс учитель
 {
-    public Teacher(int age)
-    {
-        super(age,"Teacher");
+    public Teacher(int age) {
+        super(age, "Teacher");
     }
+
     @Override
-    public void init(Scanner scanner)
-    {
+    public void init(Scanner scanner) {
         System.out.println("Input number sertificate:");
-        this.Number=scanner.nextInt();
+        this.Number = scanner.nextInt();
     }
 }
 
 class Manager extends Cooperator//класс директор
 {
-    public Manager(int age)
-    {
-        super(age,"Manager");
+    public Manager(int age) {
+        super(age, "Manager");
     }
+
     @Override
-    public void init(Scanner scanner)
-    {
+    public void init(Scanner scanner) {
         System.out.println("Input manager number:");
-        this.Number=scanner.nextInt();
+        this.Number = scanner.nextInt();
     }
 }
